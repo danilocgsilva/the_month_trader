1553d437057a8c5cf473a0383f3c46934dda3af2
Danilo Silva <danilocgsilva@gmail.com>
2022-05-22 23:27:24 -0300

    wip: organizing menus based on bulma

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 require('./styles.scss');
+
+const burgetIcon = document.querySelector('.navbar-burger')
+const navbarMenu = document.querySelector('.navbar-menu')
+
+burgetIcon.addEventListener('click', () => {
+    navbarMenu.classList.toggle('is-active')
+})
