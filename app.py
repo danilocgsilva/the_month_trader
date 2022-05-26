e0b61ca5bf5d95ad5b10e79cda9966387d2b0727
Danilo Silva <danilocgsilva@gmail.com>
2022-05-25 21:58:31 -0300

    Middleware removed

--- app.py
+++ app.py
@@ -17,9 +17,6 @@ from routes.symbol import symbol as symbol_blueprint
 # Migrations
 from flask_migrate import Migrate
 
-# Middleware
-from Middleware import Middleware
-
 # Check environment variables, so is more secure set some information
 #   as os environment variable rather than depends upon code
 import os
@@ -35,10 +32,6 @@ db_database = os.environ.get('TMT_DB_DATABASE')
 if 'TMT_DB_PORT' in os.environ:
     db_host += ":" + os.environ.get('TMT_DB_PORT')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@{}/{}'.format(db_user, db_password, db_host, db_database)
-
-# Middlware utilization. REMEMBER: this code always must be used
-#   before routes usage.
-app.wsgi_app = Middleware(app.wsgi_app)
 
 db.init_app(app)
 
